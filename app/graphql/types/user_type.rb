b1381dfaf91f8cc3a85443c0138c0d4e92d86181
joemecha <jwmecha@gmail.com>
2023-08-08 11:26:58 -0600

    Add vote to link and user types; add vote model tests

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,7 +3,10 @@
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
+    # field :created_at, DateTimeType, null: false # Rails 7 compatibility issue: uninitialized constant Types::UserType::DateTimeType
     field :name, String, null: false
     field :email, String, null: false
+    field :links, [LinkType], null: false
+    field :votes, [VoteType], null: false
   end
 end
